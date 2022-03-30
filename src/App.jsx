69a241a18514c1c4f48cb7b9c92bd2f767527e8c
Malte Hecht <malte@siggi.cloud>
2022-03-30 17:13:08 +0200

    check if everything works

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,50 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import poweredBy from "./powered-by-vitawind-dark.png";
+import useSound from "use-sound";
+import Piano from "./sounds/piano.mp3";
+
+const playbackRates = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5];
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="text-center selection:bg-green-900">
-      <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-white">
-        <img
-          src={logo}
-          className="animate-speed h-60 motion-safe:animate-spin"
-          alt="logo"
-        />
-        <style>
-          {
-            "\
-            .animate-speed{\
-              animation-duration:20s;\
-            }\
-          "
+    <div className="flex h-screen w-screen items-center justify-center gap-x-1 bg-slate-200">
+      <div className="flex items-start shadow-2xl shadow-slate-400/50">
+        {playbackRates.map((playbackRate, i) => {
+          if (i % 2 === 0) {
+            return (
+              <PianoKeyWhite key={playbackRate} playbackRate={playbackRate} />
+            );
+          } else {
+            return (
+              <PianoKeyBlack key={playbackRate} playbackRate={playbackRate} />
+            );
           }
-        </style>
-        <p className="bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent">
-          Vite + React + Tailwindcss v3
-        </p>
-        <p className="mt-3">
-          <button
-            type="button"
-            className="my-6 rounded bg-gray-300 px-2 py-2 text-[#282C34] transition-all hover:bg-gray-200"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code className="text-[#8d96a7]">App.jsx</code> and save to test
-          HMR updates.
-        </p>
-        <p className="mt-3 flex gap-3 text-center text-[#8d96a7]">
-          <a
-            className="text-[#61dafb] transition-all hover:text-blue-400"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="text-[#61dafb] transition-all hover:text-blue-400"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-        <img src={poweredBy} className="mx-auto my-8" alt="powered-by" />
-      </header>
+        })}
+      </div>
     </div>
   );
+}
+
+function PianoKeyBlack({ playbackRate }) {
+  return (
+    <PianoKey
+      playbackRate={playbackRate}
+      className="z-10 -mx-5 h-64 w-10  rounded-b border bg-black text-black"
+    />
+  );
+}
+
+function PianoKeyWhite({ playbackRate }) {
+  return (
+    <PianoKey
+      playbackRate={playbackRate}
+      className="h-96 w-16 rounded  border bg-white text-white"
+    />
+  );
+}
+
+function PianoKey({ playbackRate, className }) {
+  const [play] = useSound(Piano, { playbackRate });
+
+  return <button className={className} onClick={play} />;
 }
 
 export default App;
